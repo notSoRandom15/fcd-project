83ab0ee5e25558b63206def06dff45a76e427d92
demetre <deme.barbakadze2@gmail.com>
2023-11-16 12:26:38 +0400

    fixed bug in app file (added app name).

--- fcp/billing/urls.py
+++ fcp/billing/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from fcp.billing.views import billing
 
 
-
+app_name = 'billing'
 urlpatterns = [
-    path('', billing, name='billing'),
+    path('', billing, name='home'),
 ]
