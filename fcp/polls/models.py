0eb3faca2a0bbe6f2c624de709ec9b424eec1134
Demetre <deme.barbakadze2@gmail.com>
2023-12-21 12:31:50 +0400

    polls results fixed (needs design)

--- fcp/polls/models.py
+++ fcp/polls/models.py
@@ -1,22 +1,16 @@
 from django.db import models
-
-# Create your models here.
-
+# Question class
 class Question(models.Model):
-    question = models.CharField(max_length=300)
+    question_text = models.CharField(max_length = 300)
     pub_date = models.DateTimeField('date published')
-
-    def __str__(self):
-        return self.question
     
+    def __str__(self):
+        return self.question_text
 
-         
 class Choice(models.Model):
-    question = models.ForeignKey(Question,
-                                 on_delete=models.CASCADE)    
-    choice_text = models.CharField(max_length=300)
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default=0)
     
     def __str__(self):
         return self.choice_text
-    
